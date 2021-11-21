dd5ca1686dacbc0c52febac2ff029b0b9b55b5ad
jvs8648 <jvs8648@nyu.edu>
2021-11-21 12:56:08 -0500

    commit

---
+++ Assignment_09/app.js
@@ -0,0 +1,5 @@
+document.getElementById("Return_Button").onclick=function (){
+    location.href="https://jvs8648.github.io/Joe_Schoen_Repo.github.io/";
+};
+
+// Website that taught me how to redirect with button: https://www.codegrepper.com/code-examples/html/javascript+code+to+redirect+to+another+page+on+button+click

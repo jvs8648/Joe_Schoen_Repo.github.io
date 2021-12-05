86a608a70689fb36e5dd79ba5c3c19279681d2ee
jvs8648 <71087274+jvs8648@users.noreply.github.com>
2021-12-05 17:29:13 -0500

    Update app4.js

--- Final Project/app4.js
+++ Final Project/app4.js
@@ -4,5 +4,5 @@ document.getElementById("Noncanon_Light_Button").onclick=function (){
 };
 
 document.getElementById("Noncanon_Dark_Button").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/final_project_noncanon_dark_side.html";
+    location.href="final_project_noncanon_dark_side.html";
 };

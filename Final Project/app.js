9ac46e1e6e81845f963feae23399ddb9d3f9be7b
jvs8648 <71087274+jvs8648@users.noreply.github.com>
2021-12-05 17:27:20 -0500

    Update app.js

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,10 +1,10 @@
 //Canon Dark Side - Codes so that user will be taken to character page if they click on their name or picture
 document.getElementById("Boba_Fett_Picture").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/boba_fett.html";
+    location.href="boba_fett.html";
 };
 
 document.getElementById("Boba_Fett_Name").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/boba_fett.html";
+    location.href="boba_fett.html";
 };
 
 document.getElementById("Count_Dooku_Picture").onclick=function (){
@@ -12,29 +12,29 @@ document.getElementById("Count_Dooku_Picture").onclick=function (){
 };
 
 document.getElementById("Count_Dooku_Name").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/count_dooku.html";
+    location.href="count_dooku.html";
 };
 
 document.getElementById("Darth_Maul_Picture").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_maul.html";
+    location.href="darth_maul.html";
 };
 
 document.getElementById("Darth_Maul_Name").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_maul.html";
+    location.href="darth_maul.html";
 };
 
 document.getElementById("Darth_Vader_Picture").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_vader.html";
+    location.href="darth_vader.html";
 };
 
 document.getElementById("Darth_Vader_Name").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_vader.html";
+    location.href="darth_vader.html";
 };
 
 document.getElementById("Emperor_Palpatine_Picture").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/emperor_palpatine.html";
+    location.href="emperor_palpatine.html";
 };
 
 document.getElementById("Emperor_Palpatine_Name").onclick=function (){
-    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/emperor_palpatine.html";
+    location.href="emperor_palpatine.html";
 };

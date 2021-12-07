e7f1170cd3d6844f23474a84689bd00380030d8f
jvs8648 <jvs8648@nyu.edu>
2021-12-06 22:20:12 -0500

    commit

---
+++ Final_Project/app6.js
@@ -0,0 +1,40 @@
+//Canon Dark Side - Codes so that user will be taken to character page if they click on their name or picture
+document.getElementById("ahsoka_tano_picture").onclick=function (){
+    location.href="ahsoka_tano.html";
+};
+
+document.getElementById("ahsoka_tano_name").onclick=function (){
+    location.href="ahsoka_tano.html";
+};
+
+document.getElementById("anakin_skywalker_picture").onclick=function (){
+    location.href="anakin_skywalker.html";
+};
+
+document.getElementById("anakin_skywalker_name").onclick=function (){
+    location.href="anakin_skywalker.html";
+};
+
+document.getElementById("leia_organa_picture").onclick=function (){
+    location.href="leia_organa.html";
+};
+
+document.getElementById("leia_organa_name").onclick=function (){
+    location.href="leia_organa.html";
+};
+
+document.getElementById("obi_wan_kenobi_picture").onclick=function (){
+    location.href="obi_wan_kenobi.html";
+};
+
+document.getElementById("obi_wan_kenobi_name").onclick=function (){
+    location.href="obi_wan_kenobi.html";
+};
+
+document.getElementById("luke_skywalker_picture").onclick=function (){
+    location.href="luke_skywalker.html";
+};
+
+document.getElementById("luke_skywalker_name").onclick=function (){
+    location.href="luke_skywalker.html";
+};

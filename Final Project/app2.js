51db60d8dea7f2e552296ab2deea3565d90064c9
jvs8648 <jvs8648@nyu.edu>
2021-12-06 21:43:40 -0500

    commit

---
+++ Final Project/app2.js
@@ -0,0 +1,40 @@
+//Noncanon Dark Side - Codes so that user will be taken to character page if they click on their name or picture
+document.getElementById("Darth_Bane_Picture").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_bane.html";
+};
+
+document.getElementById("Darth_Bane_Name").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_bane.html";
+};
+
+document.getElementById("Darth_Malgus_Picture").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_malgus.html";
+};
+
+document.getElementById("Darth_Malgus_Name").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_malgus.html";
+};
+
+document.getElementById("Darth_Nihilus_Picture").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/nihilus.html";
+};
+
+document.getElementById("Darth_Nihilus_Name").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/nihilus.html";
+};
+
+document.getElementById("Darth_Plagueis_Picture").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_plagueis.html";
+};
+
+document.getElementById("Darth_Plagueis_Name").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_plagueis.html";
+};
+
+document.getElementById("Darth_Sion_Picture").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_sion.html";
+};
+
+document.getElementById("Darth_Sion_Name").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/darth_sion.html";
+};
---
+++ Final Project/app5.js
@@ -0,0 +1,8 @@
+//Canon Light/Dark choice - Code will take user to the page of the section they click on
+document.getElementById("Canon_Light_Button").onclick=function (){
+    location.href=" ";
+};
+
+document.getElementById("Canon_Dark_Button").onclick=function (){
+    location.href="file:///C:/Users/Joseph/Web_Class_C_Directory/Joe_Schoen_Repo.github.io/Final%20Project/dark_side_canon.html";
+};

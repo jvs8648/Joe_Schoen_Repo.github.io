b764ee16f2b223a5036fe2ef05d1423e77737b00
jvs8648 <jvs8648@nyu.edu>
2021-12-10 13:12:24 -0500

    commit

---
+++ Final_Project/app7.js
@@ -0,0 +1,40 @@
+//Noncanon Light Side - Codes so that user will be taken to character page if they click on their name or picture
+document.getElementById("Bastilla_Shan_Picture").onclick=function (){
+    location.href="bastilla_shan.html";
+};
+
+document.getElementById("Bastilla_Shan_Name").onclick=function (){
+    location.href="bastilla_shan.html";
+};
+
+document.getElementById("Canderous_Ordo_Picture").onclick=function (){
+    location.href="canderous_ordo.html";
+};
+
+document.getElementById("Canderous_Ordo_Name").onclick=function (){
+    location.href="canderous_ordo.html";
+};
+
+document.getElementById("Kao_Cen_Darach_Picture").onclick=function (){
+    location.href="kao_cen_darach.html";
+};
+
+document.getElementById("Kao_Cen_Darach_Name").onclick=function (){
+    location.href="Kao_Cen_Darach.html";
+};
+
+document.getElementById("Kreia_Picture").onclick=function (){
+    location.href="kreia.html";
+};
+
+document.getElementById("Kreia_Name").onclick=function (){
+    location.href="kreia.html";
+};
+
+document.getElementById("Meetra_Surik_Picture").onclick=function (){
+    location.href="meetra_surik.html";
+};
+
+document.getElementById("Meetra_Surik_Name").onclick=function (){
+    location.href="meetra_surik.html";
+};

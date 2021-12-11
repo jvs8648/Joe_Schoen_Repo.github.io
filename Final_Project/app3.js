ebd20093b54124d83fa7882a487a0a56ac808c86
jvs8648 <jvs8648@nyu.edu>
2021-12-11 13:13:29 -0500

    commit

--- Final_Project/app3.js
+++ Final_Project/app3.js
@@ -1,10 +1,12 @@
 //Canon/Noncanon choice - Code will take user to the page of the section they click on
+var audio=document.getElementById("menu")
+
 document.getElementById("Canon_Button").onclick=function (){
-  music.play("menu_sound.wav");
+  menu.play();
   location.href="canon_light_dark.html";
 };
 
 document.getElementById("Noncanon_Button").onclick=function (){
-    music.play("menu_sound.wav");
+    menu.play();
     location.href="legends_light_dark.html";
 };

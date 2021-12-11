9e8bf0d6ec774f7ba4ae398e9ee6ccf99112c42b
jvs8648 <jvs8648@nyu.edu>
2021-12-11 13:34:29 -0500

    commit

--- Final_Project/app8.js
+++ Final_Project/app8.js
@@ -2,5 +2,5 @@ const audio= document.getElementById('menu');
 
 const buttonPlay=document.getElementById('lets_go_button');
 buttonPlay.addEventListerner('click', function(){
-  aduio.play();
+  audio.play();
 });
